b787f4ee4c15f41097eeb7f73fd5648aa1232c07
Shalintha Silva <shalintha011@gmail.com>
2026-01-04 19:30:06 +0530

    Refactor the main entry C file - renamed hello.c to main.c - called greeting() and farewell() from func

--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 #include <linux/kernel.h>
 #include <linux/module.h>
+#include "func.h"
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Shalintha Silva");
@@ -8,11 +9,13 @@ MODULE_DESCRIPTION("A simple hello world module");
 static int __init hello_init(void) {
     // printk(KERN_INFO "%s: Hello, world!, From init\n", __func__);
     pr_info("%s: Hello, world!, From init\n", __func__);
+    greeting();
     return 0;
 }
 
 static void __exit hello_exit(void) {
     printk(KERN_INFO "%s: Goodbye, world!, From exit\n", __func__);
+    farewell();
 }
 
 module_init(hello_init);
